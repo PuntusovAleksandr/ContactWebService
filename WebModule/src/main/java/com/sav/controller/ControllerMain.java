9b5bc01a95ceb6876a3e1676901359478b5450fc
PuntusovAleksandr <PuntusovAleksandr@gmail.com>
2015-04-14 21:10:49 +0300

    14.04.15-01

--- WebModule/src/main/java/com/sav/controller/ControllerMain.java
+++ WebModule/src/main/java/com/sav/controller/ControllerMain.java
@@ -1,22 +1,16 @@
 package com.sav.controller;
 
 
-import allProject.entity.Contact;
-import allProject.service.ContactService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class ControllerMain {
 
-    @Autowired
-    ContactService contactService;
+//    @Autowired
+//    ContactService contactService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcomePage(){
@@ -29,12 +23,12 @@ public class ControllerMain {
         return "hello";
     }
 
-    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
-    public ModelAndView getAllContacts(){
-        ModelAndView modelAndView = new ModelAndView();
-        List<Contact> contacts = contactService.getAllContact();
-        modelAndView.addObject("contacts", contacts);
-        modelAndView.setViewName("contacts");
-        return modelAndView;
-    }
+//    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
+//    public ModelAndView getAllContacts(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        List<Contact> contacts = contactService.getAllContact();
+//        modelAndView.addObject("contacts", contacts);
+//        modelAndView.setViewName("contacts");
+//        return modelAndView;
+//    }
 }
